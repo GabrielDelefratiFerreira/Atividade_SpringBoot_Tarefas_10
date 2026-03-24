f20c84e6aa4b649717cf48a3c38f2623fc7b5699
Gabriel Delefrati <168195936+GabrielDelefratiFerreira@users.noreply.github.com>
2026-03-24 12:00:20 -0400

    Model criada

---
+++ tarefas/src/main/java/com/example/tarefas/Model/TarefaModel.java
@@ -0,0 +1,55 @@
+package com.example.tarefas.Model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_tarefa")
+public class TarefaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String descricao;
+
+    @Column(name = "Data_vencimento_da_tarefa")
+    private LocalDate dataVencimento;
+    @Column(name = "Status_da_tarefa")
+    private Boolean concluida;
+
+    public TarefaModel(){
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(LocalDate dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    public Boolean getConcluida() {
+        return concluida;
+    }
+
+    public void setConcluida(Boolean concluida) {
+        this.concluida = concluida;
+    }
+}
